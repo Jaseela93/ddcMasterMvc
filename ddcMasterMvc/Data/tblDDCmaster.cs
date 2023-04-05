6148c6b04dc6c1b11d1a81ff7ce5ad842db814d2
Jaseela93 <jaseelajamal93@gmail.com>
2023-04-05 13:07:23 +0400

    April 5

---
+++ ddcMasterMvc/Data/tblDDCmaster.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.SqlClient;
+
+namespace ddcMasterMvc.Data
+{
+    public class tblDDCmaster
+    {
+        public string connectionString { get; set; }
+
+        public string getDHAcode(string mohapCode)
+        {
+            // Create a connection to the database
+            connectionString = "Data Source=localhost;Initial Catalog=DDC_Master;User ID=myUsername;Password=myPassword;Integrated Security=True;MultipleActiveResultSets=True;";
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            // Open the connection
+            connection.Open();
+
+            // Create a command to execute a SQL query
+            SqlCommand command = new SqlCommand("SELECT Drug_DHA FROM tbl_ddc_master$ WHERE Drug_MOHAP = '" + mohapCode +"'", connection);
+
+            // Execute the query and read the results
+            string code = (string)command.ExecuteScalar();
+                      
+            connection.Close();
+            return code;
+
+        }
+
+    }
+}
--- ddcMasterMvc/Models/ddcConvrter.cs
+++ ddcMasterMvc/Models/ddcConvrter.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml;
+﻿using ddcMasterMvc.Data;
+using System.Xml;
 
 namespace ddcMasterMvc.Models
 {
@@ -7,8 +8,10 @@ namespace ddcMasterMvc.Models
         public IFormFile File { get; set; }
         public string FExtension { get; set; }
         public string FPath { get; set; }
+        public string mohapCode { get; set; } = string.Empty;
         public void ConvertDDC()
         {
+            tblDDCmaster ob = new tblDDCmaster();
             if (File != null && File.Length > 0)
             {
                 
@@ -28,7 +31,12 @@ namespace ddcMasterMvc.Models
                             {
                                 if(codeNode.InnerText.Length >15)
                                 {
-                                    codeNode.InnerText = "New value";
+                                    mohapCode = codeNode.InnerText;
+                                    if(mohapCode != string.Empty)
+                                    {
+                                        codeNode.InnerText = ob.getDHAcode(mohapCode);
+                                    }
+                                                                       
                                 }
                                 
                             }
